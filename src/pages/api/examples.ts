e40a433037e95ee3cc1e67eaf8fee47ef471d393
Daniel <danielframos09@gmail.com>
2022-12-18 21:00:41 -0600

    fix: change route from 'example' to 'card' model to avoid crash

--- src/pages/api/examples.ts
+++ src/pages/api/examples.ts
@@ -2,8 +2,10 @@ import { type NextApiRequest, type NextApiResponse } from "next";
 
 import { prisma } from "../../server/db/client";
 
+// NOTE: This route is for reference
+
 const examples = async (req: NextApiRequest, res: NextApiResponse) => {
-  const examples = await prisma.example.findMany();
+  const examples = await prisma.card.findMany();
   res.status(200).json(examples);
 };
 
